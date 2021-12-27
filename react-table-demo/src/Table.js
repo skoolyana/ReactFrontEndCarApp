83dd5d9e5767b56346ff29c2472a754bbfc75577
unknown <kulyanasun@gmail.com>
2021-12-27 18:45:27 +0100

    Added Filter and Sort for React Table Example

--- react-table-demo/src/Table.js
+++ react-table-demo/src/Table.js
@@ -1,7 +1,24 @@
 import React from "react";
 import { useTable } from "react-table";
+import { useState } from "react";
+import { useFilters } from "react-table";
+import { useSortBy } from "react-table";
 
 export default function Table({ columns, data }) {
+
+
+// Create a state
+const [filterInput, setFilterInput] = useState("");
+
+// Update the state when input changes
+const handleFilterChange = e => {
+  const value = e.target.value || undefined;
+  setFilter("show.name", value); // Update the show.name filter. Now our table will filter and show only the rows which have a matching value
+  setFilterInput(value);
+};
+
+
+
   // Use the useTable Hook to send the columns and data to build the table
   const {
     getTableProps, // table props from react-table
@@ -9,22 +26,44 @@ export default function Table({ columns, data }) {
     headerGroups, // headerGroups, if your table has groupings
     rows, // rows for the table based on the data passed
     prepareRow, // Prepare the row (this function needs to be called for each row before getting the row props)
+    setFilter, // The useFilter Hook provides a way to set the filter
   } = useTable({
     columns,
     data,
-  });
+    },
+    useFilters,
+    useSortBy // This plugin Hook will help to sort our table columns
+    );
 
 /* 
     Render the UI for your table
     - react-table doesn't have UI, it's headless. We just need to put the react-table props from the Hooks, and it will do its magic automatically
   */
     return (
+      <>
+      <input
+        value={filterInput}
+        onChange={handleFilterChange}
+        placeholder={"Search name"}
+      />
+      
         <table {...getTableProps()}>
           <thead>
             {headerGroups.map(headerGroup => (
               <tr {...headerGroup.getHeaderGroupProps()}>
                 {headerGroup.headers.map(column => (
-                  <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+                 <th
+                 {...column.getHeaderProps(column.getSortByToggleProps())}
+                 className={
+                   column.isSorted
+                     ? column.isSortedDesc
+                       ? '🔽'
+                       : '🔼'
+                     : ''
+                 }
+               >
+                 {column.render("Header")}
+               </th>
                 ))}
               </tr>
             ))}
@@ -42,6 +81,7 @@ export default function Table({ columns, data }) {
             })}
           </tbody>
         </table>
+        </>
       );
 
 
